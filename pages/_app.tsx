98c2adbbfdd475048f16f8dd8243a1e96c4db369
A.Ehyaei <A.Ehyaei@flytoday.ir>
2022-12-20 19:43:47 +0330

    feat: initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/typography.scss';
+import '../styles/globals.scss';
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return <Component {...pageProps} />;
 }
