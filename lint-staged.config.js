c1e56e77dc73e0447c5776c4734fd2994fcb84bb
Ali <aliehyaie9469@gmail.com>
2022-12-30 15:21:17 +0330

    fix: lint-stage issue with space in folder name

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -4,11 +4,17 @@ module.exports = {
 
     // This will lint and format TypeScript and                                             //JavaScript files
     '**/*.(ts|tsx|js)': filenames => [
-        `yarn eslint --fix ${filenames.join(' ')}`,
-        `yarn prettier --write ${filenames.join(' ')}`,
+        `yarn eslint --fix ${filenames
+            .map(filename => `"${filename}"`)
+            .join(' ')}`,
+        `yarn prettier --write ${filenames
+            .map(filename => `"${filename}"`)
+            .join(' ')}`,
     ],
 
     // this will Format MarkDown and JSON
     '**/*.(md|json)': filenames =>
-        `yarn prettier --write ${filenames.join(' ')}`,
+        `yarn prettier --write ${filenames
+            .map(filename => `"${filename}"`)
+            .join(' ')}`,
 };
