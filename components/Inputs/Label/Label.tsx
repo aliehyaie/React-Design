f63808b49c70faf0d574851e01272c09cc9aff26
Ali <aliehyaie9469@gmail.com>
2023-05-16 22:25:35 +0330

    feat: create input label component

---
+++ components/Inputs/Label/Label.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { twMerge } from 'tailwind-merge';
+import Text from '../../Text/Text';
+import { ILabel } from './ILabel';
+
+const Label: React.FC<ILabel> = ({
+    error,
+    label,
+    labelClassName,
+    required,
+}) => {
+    return (
+        <Text
+            className={twMerge(
+                `inline-block self-start relative ${error && 'text-error'}`,
+                labelClassName
+            )}
+        >
+            {label || ''}
+            {required && (
+                <Text
+                    variant='span'
+                    className='absolute -top-1 -left-2 block h-1.5 w-1.5 rounded-full bg-error'
+                />
+            )}
+        </Text>
+    );
+};
+
+export default Label;
