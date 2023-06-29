e6f9e679af37a415f74f518103584e96cc11dba6
Ali <aliehyaie9469@gmail.com>
2023-06-29 18:27:20 +0330

    fix: ''

--- utils/validations/messages/messages.ts
+++ utils/validations/messages/messages.ts
@@ -3,6 +3,3 @@ export const requiredMessage = (name?: string) =>
 
 export const invalidFormatMessage = (name?: string) =>
     ` لطفا ${name || 'مقدار'} معتبر وارد کنید.  `;
-
-export const invalidFormatMessage2 = (name?: string) =>
-    ` لطفا ${name || 'مقدار'} معتبر وارد کنید.  `;
