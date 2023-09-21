c31909c7c06adcbd0eae970af5cf6ea4701eeb60
Ali Ehyaie <aliehyaie9469@gmail.com>
2023-09-21 16:50:28 +0330

    feat: add useOnClickOutside hook

---
+++ hooks/useOnClickOutside.tsx
@@ -0,0 +1,27 @@
+import { RefObject, useEffect } from 'react';
+
+const useOnClickOutside = <T extends HTMLElement = HTMLElement>(
+    ref: RefObject<T>,
+    handler: (event: MouseEvent) => void,
+    mouseEvent: 'mousedown' | 'mouseup' = 'mousedown'
+): void => {
+    useEffect(() => {
+        const listener = (event: MouseEvent) => {
+            const el = ref?.current;
+
+            // Do nothing if clicking ref's element or descendent elements
+            if (!el || el.contains(event.target as Node)) {
+                return;
+            }
+            handler(event);
+        };
+
+        document.addEventListener(mouseEvent, listener);
+
+        return () => {
+            document.removeEventListener(mouseEvent, listener);
+        };
+    }, [ref, handler, mouseEvent]);
+};
+
+export default useOnClickOutside;
