49eaa0a411477cf8faf598d9d9815feb7dc166f0
Ali Ehyaie <aliehyaie9469@gmail.com>
2023-09-28 21:50:58 +0330

    refactor: change loader component to loading

--- components/Loading/Loading.tsx
+++ components/Loading/Loading.tsx
@@ -1,10 +1,10 @@
 import React, { FC } from 'react';
 import classes from './Loader.module.scss';
 import { twMerge } from 'tailwind-merge';
-import { ILoader } from './ILoader';
+import { ILoading } from './ILoading';
 
-const Loader: FC<ILoader> = ({ className }) => {
+const Loading: FC<ILoading> = ({ className }) => {
     return <div className={twMerge(classes.dotFlashing, className)} />;
 };
 
-export default Loader;
+export default Loading;
